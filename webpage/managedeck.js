cc3d5300f179855bb7e3222dbe61f3e40d915a58
knangcas <knangcas@asu.edu>
2025-02-20 10:02:57 -0600

    refactored a lot of code with scope variables, and updated logic with buttons being active/inactive

--- webpage/managedeck.js
+++ webpage/managedeck.js
@@ -2,8 +2,12 @@ let cardArray = [];
 let cards = "";
 let cardQty = 0;
 let newCardActive = true;
-let editCardActive = false;
-
+const button = document.getElementById("saveButton");
+const  listElement = document.getElementById("cardList");
+const  questionField = document.getElementById("questionTextArea");
+const  answerField = document.getElementById("answerTextArea");
+const  cancelButton = document.getElementById("cancelButton");
+const  newCard = document.getElementById("newCard");
 async function postData(questionText, answerText) {
     const url = "http://localhost:8080/flashcards";
 
@@ -50,13 +54,11 @@ async function getData() {
 async function populateList() {
     listElement = document.getElementById("cardList");
     cards = JSON.parse(await getData());
-
     for(let i = 0; i < cards.cards.length; i++) {
         cardArray[i] = {question: cards.cards[i].question, answer: cards.cards[i].answer, cardID : cards.cards[i].cardID}
     }
     cardQty = cardArray.length;
     document.getElementById("cardQty").innerText = `${cardQty} Cards`;
-
     listElement.appendChild(new Option(cardArray[0].question, "0", true, false));
     if (cardArray.length > 1) {
         for (let i = 1; i < cardArray.length; i++) {
@@ -65,19 +67,8 @@ async function populateList() {
     }
     document.getElementById("saveButton").disabled = true;
     console.log(listElement.selectedIndex);
-
-
 }
 
-function editButtonChange() {
-    let button = document.getElementById("saveButton");
-
-    if (button.disabled) {
-        button.disabled = false;
-    } else {
-        button.disabled = true;
-    }
-}
 
 function populateFields() {
     newCardActive = false;
@@ -88,10 +79,6 @@ function populateFields() {
     let cancelButton = document.getElementById("cancelButton");
     cancelButton.disabled = true;
     console.log("populateFields called");
-    let questionField = document.getElementById("questionTextArea");
-    let answerField = document.getElementById("answerTextArea");
-    let listElement = document.getElementById("cardList");
-
     let index = Number(listElement.value)
     console.log(cardArray[index].question)
     questionField.value = cardArray[index].question;
@@ -109,74 +96,54 @@ async function updateData(id,questionText,answerText) {
         if (!response.ok) { throw new Error(`Response Status : ${response.status}`)};
         const json = await response.json();
         console.log(json);
-        //return JSON.stringify(json);
     } catch (error) {
         console.error(error.message);
     }
 }
 
 function changeTextAreaActive() {
-    let questionArea = document.getElementById("questionTextArea");
-    let answerArea = document.getElementById("answerTextArea");
-
-    if (answerArea.disabled || questionArea.disabled) {
-        answerArea.disabled = false;
-        questionArea.disabled = false;
-    }
+        answerField.disabled = false;
+        questionField.disabled = false;
 }
 
 function changeTextAreaInActive() {
-    let questionArea = document.getElementById("questionTextArea");
-    let answerArea = document.getElementById("answerTextArea");
-
-
-    answerArea.disabled = true;
-    questionArea.disabled = true;
-
+    answerField.disabled = true;
+    questionField.disabled = true;
 }
 function changedTextArea() {
-    let cancelButton = document.getElementById("cancelButton");
-    let saveButton = document.getElementById("saveButton");
-    let questionArea = document.getElementById("questionTextArea");
-    let answerArea = document.getElementById("answerTextArea");
     if (!newCardActive) {
         cancelButton.disabled = false;
-
+        if (questionField.value !== cardArray[listElement.selectedIndex].question || answerField.value !== cardArray[listElement.selectedIndex].answer) {
+            saveButton.disabled = false;
+        } else {
+            saveButton.disabled = true;
+        }
     } else {
         saveButton.disabled = false;
-        if (questionArea.value.length === 0 || answerArea.value.length === 0) {
+        if (questionField.value.length === 0 || answerField.value.length === 0) {
             saveButton.disabled = true;
         }
     }
 
-
-
-
 }
 
 function editCard() {
-    let button = document.getElementById("saveButton");
+    listElement.disabled = true;
     changeTextAreaActive();
     button.disabled = true;
     button.innerText = "Save Card";
     button.setAttribute("onclick", "saveCard()");
-    document.getElementById("cancelButton").disabled = false;
-
+    cancelButton.disabled = false;
+    newCard.disabled = true;
 
 }
 
 function enableSaveButton() {
-    let button = document.getElementById("saveButton");
     button.disabled = false;
     button.innerText = "Save Card";
     button.setAttribute("onclick", "saveCard()");
 }
 function saveCard() {
-    let button = document.getElementById("saveButton");
-    let questionField = document.getElementById("questionTextArea");
-    let answerField = document.getElementById("answerTextArea");
-    let listElement = document.getElementById("cardList");
-
     let index = Number(listElement.value);
     if (!newCardActive) {
         listElement.options[listElement.selectedIndex].innerText = questionField.value;
@@ -185,23 +152,23 @@ function saveCard() {
         listElement.appendChild(new Option(questionField.value, (cardArray.length + 1).toString(), false, false));
         postData(questionField.value, answerField.value);
     }
+    newCard.disabled = false;
+    listElement.disabled = false;
+    button.disabled = true;
+    changeToEditButton();
+    cancelButton.disabled = false;
 
 }
 
-function newCard() {
+function addNewCard() {
     newCardActive = true;
-
+    newCard.disabled = true;
     changeTextAreaActive();
     enableSaveButton();
-    document.getElementById("saveButton").disabled = true;
-    //todo work on disabling buttons given a state
-    let cancelButton = document.getElementById("cancelButton");
+    button.disabled = true;
     cancelButton.disabled = false;
-    let listElement = document.getElementById("cardList");
     listElement.selectedIndex = -1;
     listElement.disabled = true;
-    let questionField = document.getElementById("questionTextArea");
-    let answerField = document.getElementById("answerTextArea");
     questionField.value = "";
     answerField.value = "";
     questionField.placeholder = "Enter your question here...";
@@ -210,19 +177,14 @@ function newCard() {
 }
 
 function cancelCard() {
-    let questionField = document.getElementById("questionTextArea");
-    let answerField = document.getElementById("answerTextArea");
-    let listElement = document.getElementById("cardList");
     listElement.disabled = false;
-    if (newCardActive) {
-        changeToEditButton();
-        document.getElementById("cancelButton").disabled = true;
-    }
+    newCard.disabled = false;
+    changeToEditButton();
+    cancelButton.disabled = true;
     changeTextAreaInActive();
     listElement.selectedIndex = -1;
     questionField.value = "";
     answerField.value = "";
-    document.getElementById("cancelButton").disabled = true;
 }
 
 function changeToEditButton(){
