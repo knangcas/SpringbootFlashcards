0ac9a81a2bad2b42c8d00f14ffd8590d9e92ca5b
knangcas <knangcas@asu.edu>
2025-02-22 08:58:14 -0600

    added more flashcards

--- src/main/java/com/FlashCardsSPA/LoadDatabase.java
+++ src/main/java/com/FlashCardsSPA/LoadDatabase.java
@@ -25,7 +25,10 @@ class LoadDatabase {
             log.info("Preloading " + cardRepo.save(new FlashCard( "What is the difference between --- and == in javascript?", "=== is more strict and does not do type conversions like == does", 1)));
             log.info("Preloading " + cardRepo.save(new FlashCard("What is the left most digit in the three digit HTTP status code for client related errors?", "4xx", 1)));
             log.info("Preloading " + cardRepo.save(new FlashCard("What is penetration testing?", "Testing done by ethical hackers in which the goal is to try and breach a system.", 1)));
-
+            log.info("Preloading " + cardRepo.save(new FlashCard("If you have a child, will you name them JSON?", "Of course.", 1)));
+            log.info("Preloading " + cardRepo.save(new FlashCard("True or False. C# uses PascalCase for naming methods", "True", 1)));
+            log.info("Preloading " + cardRepo.save(new FlashCard("What does de-referencing a pointer do?" , "retrieves the value stored at the pointer location", 1)));
+            log.info("Preloading " + cardRepo.save(new FlashCard("What is polymorphism?", "Polymorphism is when a subclass overrides methods of it's superclass", 1)));
         };
     }
 }
