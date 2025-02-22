e78e6402fd39b13f95bad569cf2b60ccdcc67183
knangcas <knangcas@asu.edu>
2025-02-22 10:54:18 -0600

    refactored index.js and fixed bugs with skipped deck adding to itself

--- webpage/javascript/index.js
+++ webpage/javascript/index.js
@@ -1,86 +1,62 @@
-console.log('Thank you for using WebStorm 💙')
 let cardArray = [];
-let aArray = [];
-let x = 0;
 let cards = "";
 let qIndex = 0;
-let progress = 0;
 let title = "";
 let skipped = [];
 let skippedQty = 0;
 let end = false;
+const progressText = document.getElementById("progress");
+const deckNameText = document.getElementById("deckNameH1");
+const content = document.getElementById('flashCardContent');
+const skippedElement = document.getElementById('skippedSpan');
+const nextButton = document.getElementById("nextButton");
+const skipButton = document.getElementById("skipButton");
 
 async function getData() {
     const url = "http://localhost:8080/flashcarddecks/1";
-
     try {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
-
         const json = await response.json();
         console.log(json);
         return JSON.stringify(json);
-
     } catch (error) {
         console.error(error.message);
     }
-
-
 }
-
-function lmao(x2) {
-    const link = document.getElementById('tester');
-    console.log("oh cool");
-    x++;
-    console.log(x2);
-    link.innerHTML = x2.name + x.toString();
-
-
-}
-
 
 
 async function load() {
-    const element = document.getElementById('flashCardContent');
-
     const question1 = getData();
-
     cards = JSON.parse(await question1);
     console.log(cards.cards[0].question);
     for (let i = 0; i < cards.cards.length; i++) {
         cardArray[i] = {question: cards.cards[i].question, answer: cards.cards[i].answer}
-        //aArray[i] = q.cards[i].answer;
     }
-
     title = cards.name;
-
     updateName();
-
-
-
-    document.getElementById('progress').innerText = "Card " + (qIndex+1) + " out of " + cardArray.length;
-
-
-    console.log(aArray);
-
-
-    element.innerHTML = cardArray[0].question;
-
+    progressText.innerText = "Card " + (qIndex+1) + " out of " + cardArray.length;
+    content.innerHTML = cardArray[0].question;
 }
 
 function loadSkippedCards() {
     cardArray = null;
-    cardArray = skipped;
+    cardArray = []
+    for (let i = 0; i < skipped.length; i++) {
+        cardArray[i] = skipped[i];
+    }
+    skipped = null;
+    skipped = [];
+    end = false;
     skippedQty = 0;
     console.log(cardArray);
-    document.getElementById("skippedSpan").hidden = true;
+    skipButton.disabled = false;
+    skippedElement.hidden = true;
 }
 
 function nextCard() {
-    const nextButton = document.getElementById('nextButton');
-
     if (end) {
         loadSkippedCards();
         nextButton.innerText = "Next";
@@ -89,28 +65,24 @@ function nextCard() {
         updateProgress()
 
     }
-
-    const element = document.getElementById('flashCardContent');
     if (qIndex === cardArray.length - 1) {
-        element.innerHTML = "You have reached the end!";
+        content.innerHTML = "You have reached the end!";
         end = true;
+        skipButton.disabled = true;
         endDeck();
         return;
     }
-
-    element.innerHTML = cardArray[++qIndex].question
+    content.innerHTML = cardArray[++qIndex].question
     updateProgress()
 }
 
 function endDeck() {
-    const element = document.getElementById('nextButton');
     if (skippedQty > 0) {
-        element.innerHTML = "Load skipped cards";
+        nextButton.innerHTML = "Load skipped cards";
     } else {
-        document.getElementById("skipButton").disabled = true;
-        element.disabled = true;
+        skipButton.disabled = true;
+        nextButton.disabled = true;
     }
-
 }
 
 function restart() {
@@ -123,54 +95,50 @@ function restart() {
     skipped = null;
     skippedQty = 0;
     end = false;
-    document.getElementById("skippedSpan").hidden = true;
-    const element = document.getElementById('flashCardContent');
-    element.innerText = cardArray[++qIndex].question;
-    document.getElementById("nextButton").innerText = "Next";
-    document.getElementById("nextButton").disabled = false;
-    document.getElementById("skipButton").disabled = false;
+    skippedElement.hidden = true;
+    content.innerText = cardArray[++qIndex].question;
+    nextButton.innerText = "Next";
+    nextButton.disabled = false;
+    skipButton.disabled = false;
     updateProgress();
-
 }
 
 function skip(){
     if (end) {
         return;
     }
-    const element = document.getElementById('flashCardContent');
-    const skippedElement = document.getElementById('skippedSpan');
     skippedElement.hidden = false;
     skippedElement.innerHTML = ++skippedQty;
     skipped.push(cardArray[qIndex])
     if (qIndex === cardArray.length - 1) {
-        element.innerHTML = "You have reached the end!";
+        content.innerHTML = "You have reached the end!";
         end = true;
+        skipButton.disabled = true;
         endDeck();
         return;
     }
-    element.innerHTML = cardArray[++qIndex].question
-
+    content.innerHTML = cardArray[++qIndex].question
     updateProgress();
 
 }
 
 function flipCard() {
-    const content = document.getElementById('flashCardContent');
+    if (end) {
+        return;
+    }
     if (content.innerText === cardArray[qIndex].question) {
         content.innerHTML = cardArray[qIndex].answer;
     } else {
         content.innerHTML = cardArray[qIndex].question
     }
-
 }
 
 function updateProgress() {
-    document.getElementById('progress').innerText = "Card " + (qIndex+1) + " out of " + cardArray.length;
+    progressText.innerText = "Card " + (qIndex+1) + " out of " + cardArray.length;
 }
 
 function updateName() {
-    document.getElementById("deckNameH1").innerText = title;
+    deckNameText.innerText = title;
 }
-
 
 load()
